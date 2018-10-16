8c059db374ed2b046857cd802d37e41bf4093d97
daemonna <32168685+peterducai@users.noreply.github.com>
2018-10-16 16:31:27 +0200

    Update install_mls.sh

--- install_mls.sh
+++ install_mls.sh
@@ -5,3 +5,5 @@ yum install selinux-policy-mls policycoreutils-python -y
 useradd -Z user_u john
 passwd john
 semanage login --modify --seuser user_u --range s2:c100 john
+chcon -R -l s2:c100 /home/john #correct home folder label
+semanage login -l
